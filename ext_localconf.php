3463616a18dada232764207c68f3cb748f930679
David Geib <d.geib@freesixtyfive.de>
2021-01-15 10:23:26 +0100

    icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -41,7 +41,7 @@ call_user_func(
 			$iconRegistry->registerIcon(
 				'fsf_zoller_slidertext-plugin-zollerslidertext',
 				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-				['source' => 'EXT:fsf_zoller_slidertext/Resources/Public/Icons/user_plugin_zollerslidertext.svg']
+				['source' => 'EXT:fsf_zoller_slidertext/Resources/Public/Icons/zoller_z.svg']
 			);
 		
     }
